dcdcc280e03addf4c12c59622eb3dc8fe263e17d
Dhiraj Mishra <work.dhirajmishra@gmail.com>
2025-06-09 10:52:46 +0530

    Initial portfolio commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import eduTechImg from "../../Assets/Projects/eduTechImg.png";
-import ticTacToeImg from "../../Assets/Projects/ticTacToeImg.png";
-import randomPassImg from "../../Assets/Projects/randomPassImg.png";
-import bgmiProjectImg from "../../Assets/Projects/bgmiProjectImg.png";
-import parallaxImg from "../../Assets/Projects/parallaxImg.png";
-import shoppingCartImg from "../../Assets/Projects/shoppingCartImg.png";
+
+import clothConnectImg from "../../Assets/Projects/clothConnectImg.png"; 
+import expenseTrackerImg from "../../Assets/Projects/expenseTrackerImg.png";
+import devTinderImg from "../../Assets/Projects/devTinderImg.png"; 
 
 function Projects() {
   return (
@@ -15,80 +13,46 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Recent <strong className="purple">Works</strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bgmiProjectImg}
+              imgPath={clothConnectImg}
               isBlog={false}
-              title="BGMI Tournaments "
-              description="Developed a web application to streamline the process of organizing and participating in BGMI (Battlegrounds Mobile India) tournaments."
-              ghLink="https://github.com/monk0707/bgmi-tournaments"
-              demoLink="https://www.bgmitournaments.in/"
+              title="ClothConnect"
+              description="A full-stack web application focused on managing clothing donations efficiently. Features include user authentication, donation tracking, NGO integrations, and a dashboard for analytics."
+              ghLink="https://github.com/dhirajmishra11/clothConnect"
+              demoLink="https://clothconnect.onrender.com"
+            />
+          </Col>
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={expenseTrackerImg}
+              isBlog={false}
+              title="Expense Tracker"
+              description="A user-friendly expense tracking app to help users manage their finances by logging income and expenses, generating reports, and visualizing spending habits."
+              ghLink="https://github.com/dhirajmishra11/expense-tracker"
+              demoLink="https://expense-tracker-as4o.onrender.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={shoppingCartImg}
+              imgPath={devTinderImg}
               isBlog={false}
-              title="SHOPPING CART"
-              description="Developed a versatile and responsive shopping cart application in which users can add and delete items in the cart."
-              ghLink="https://github.com/monk0707/shopping-cart"
-              demoLink="https://lucky-nougat-376edf.netlify.app/"
+              title="DevTinder"
+              description="A platform designed for developers to connect, collaborate, and find projects or job opportunities tailored to their skills and interests."
+              ghLink="#" // add GitHub link when available
+              demoLink="#" // add demo link when available
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={randomPassImg}
-              isBlog={false}
-              title="RANDOM PASSWORD GENERATOR"
-              description="•	One can set constraints to generate the password like including uppercase letters, lowercase letters, Numbers and Symbols.
-•	One can also set the length of the password to generate and we can also get to know whether the generated password is strong or weak.
-"
-              ghLink="https://github.com/monk0707/random-password-generator"
-              demoLink=" https://monk0707.github.io/random-password-generator/"            
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={eduTechImg}
-              isBlog={false}
-              title="EduConnect"
-              description="EduConnect is a cutting-edge educational platform built using the MERN stack (MongoDB, Express.js, React, Node.js), designed to revolutionize online learning. This platform offers a seamless, interactive, and engaging learning experience for students and educators alike. With features like real-time collaboration, personalized learning paths, and an intuitive user interface, EduConnect bridges the gap between traditional classrooms and modern digital education. Whether you're looking to enhance your skills or share your knowledge, EduConnect provides the tools and community to support your educational journey."
-              demoLink="https://study-notion-edtech-frontend-pink.vercel.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={parallaxImg}
-              isBlog={false}
-              title="Parallax Effect"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/monk0707/parallax-effect"
-              demoLink="https://monk0707.github.io/parallax-effect/"
-              //  <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={ticTacToeImg}
-              isBlog={false}
-              title="TicTacTwist"
-              description="TicTacTwist is a modern take on the classic Tic Tac Toe game, crafted with JavaScript and styled with Tailwind CSS for a sleek and responsive design. This game offers a delightful user experience with smooth animations, intuitive gameplay, and a minimalist interface. Whether you're playing against a friend or challenging the computer, TicTacTwist provides a fun and engaging way to enjoy this timeless game, all from the convenience of your web browser. Dive into a world where strategy meets style, and see if you can outsmart your opponent in this reimagined classic!"
-              ghLink="https://github.com/monk0707/tictac"
-              demoLink="https://monk0707.github.io/tictac/"      
-              // <--------Please include a demo link here 
-            />
-          </Col>
+          
         </Row>
       </Container>
     </Container>
